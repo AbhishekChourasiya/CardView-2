fb2275771b3a140294c02a7fbdcbd23c89acf0e5
Sachi Reddy <sachin.kasaraddi@healthgraph.in>
2015-03-29 11:04:16 +0530

    Initial commit

---
+++ app/src/main/java/cardview/healthgraph/com/cardview/ContactInfo.java
@@ -0,0 +1,15 @@
+package cardview.healthgraph.com.cardview;
+
+/**
+ * Created by Sachi Reddy on 3/29/2015.
+ */
+public class ContactInfo {
+    protected String name;
+    protected String surname;
+    protected String email;
+
+
+    protected static final String NAME_PREFIX = "Name_";
+    protected static final String SURNAME_PREFIX = "FullName";
+    protected static final String EMAIL_PREFIX = "email_";
+}
